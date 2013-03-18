43b5a750589c40c2f488d79385c4f18e155f149c
Ilan Schnell <ilanschnell@gmail.com>
2013-03-18 16:46:25 -0500

    merge into single top-level module

--- conda_api.py
+++ conda_api.py
@@ -1,11 +1,29 @@
 import re
 import os
+import sys
+from subprocess import Popen, PIPE
 from os.path import isdir, join
-
-import utils
 
 
 ROOT_PREFIX = '/opt/anaconda'
+
+
+def _call_conda(prefix, extra_args):
+    # call conda with the list of extra arguments, and return the tuple
+    # stdout, stderr
+    if sys.platform == 'win32':
+        python = join(ROOT_PREFIX, 'python.exe')
+        conda = join(ROOT_PREFIX, 'Scripts', 'conda-script.py')
+    else:
+        python = join(ROOT_PREFIX, 'bin/python')
+        conda = join(ROOT_PREFIX, 'bin/conda')
+
+    args = [python, conda] + extra_args
+    try:
+        p = Popen(args, stdout=PIPE, stderr=PIPE)
+    except OSError:
+        sys.exit("Error: could not invoke %r\n" % args)
+    return p.communicate()
 
 
 def set_root_prefix(prefix):
@@ -21,7 +39,7 @@ def get_conda_version():
     return the version of conda being used as a string
     """
     pat = re.compile(r'conda:?\s+(\d\.\d\S+)')
-    stdout, stderr = utils.call_conda(ROOT_PREFIX, ['--version'])
+    stdout, stderr = _call_conda(ROOT_PREFIX, ['--version'])
     m = pat.match(stderr.strip())
     if m is None:
         raise Exception('output did not match: %r' % stderr)
@@ -40,7 +58,7 @@ def get_envs():
 
 def linked(prefix):
     """
-    Return the (set of canonical names) of linked packages in prefix.
+    return the (set of canonical names) of linked packages in prefix
     """
     meta_dir = join(prefix, 'conda-meta')
     if not isdir(meta_dir):
@@ -48,10 +66,17 @@ def linked(prefix):
     return set(fn[:-5] for fn in os.listdir(meta_dir) if fn.endswith('.json'))
 
 
+def split_canonical_name(cname):
+    """
+    split a canonical package name into (name, version, build) strings
+    """
+    return tuple(cname.rsplit('-', 2))
+
+
 if __name__ == '__main__':
-    #set_root_prefix('/Users/ilan/python')
-    print repr(get_conda_version())
+    set_root_prefix('/Users/ilan/python')
+    print(repr(get_conda_version()))
     for prefix in get_envs():
-        print prefix
+        print(prefix)
         for dist in linked(prefix):
-            print '\t', dist
+            print('\t' + dist)
