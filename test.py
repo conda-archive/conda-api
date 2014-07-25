f1b439f5178f83b9db0cfea0b55c272168d95850
David Li <li.davidm96@gmail.com>
2014-07-25 17:15:31 -0500

    Add basic tests

---
+++ test.py
@@ -0,0 +1,39 @@
+import shutil
+import tempfile
+import unittest
+
+import conda_api
+
+# Test Conda-Api by performing operations in a temporary directory
+
+class TestApi(unittest.TestCase):
+    @classmethod
+    def setUpClass(self):
+        conda_api.set_root_prefix()
+        self.prefix = tempfile.mkdtemp()
+
+    @classmethod
+    def tearDownClass(self):
+        shutil.rmtree(self.prefix)
+
+    def test_version(self):
+        try:
+            conda_api.get_conda_version()
+        except Exception as e:
+            self.fail("get_conda_version fails: %s" % e)
+
+    def test_envs(self):
+        self.assertIsInstance(conda_api.get_envs(), list)
+
+# print(conda_api.config_path())
+# print(conda_api.config_path(system=True))
+# print(conda_api.config_get())
+# print(conda_api.config_get('channels'))
+# print(conda_api.config_get('use_pip'))
+# print(conda_api.config_set('use_pip', True))
+# print(conda_api.config_get('use_pip'))
+# print(conda_api.config_set('use_pip', False))
+# print(conda_api.config_get('use_pip'))
+# print(conda_api.config_add('channels', 'binstar'))
+# print(conda_api.config_remove('channels', 'binstar'))
+# print(conda_api.config_delete('use_pip'))
